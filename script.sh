afe44bc9a05cbb2aec49f80f63363fc3d36a9dfa
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2025-07-04 10:43:25 -0600

    n

--- script.sh
+++ script.sh
@@ -1,1 +1,39 @@
 
+#!/bin/bash
+
+# Make scripts executable
+chmod +x polkit.sh display.sh tools.sh pre-install.sh winmans.sh network.sh gtk.sh /BackUps/mvit.sh webbrowsers.sh lightdm.sh
+
+# Execute scripts sequentially
+./polkit.sh
+wait
+
+./display.sh
+wait
+
+./tools.sh
+wait
+
+./pre-install.sh
+wait
+
+./winmans.sh
+wait
+
+./network.sh
+wait
+
+./gtk.sh
+wait
+
+./BackUps/mvit.sh
+wait
+
+./webbrowsers.sh
+wait
+
+./lightdm.sh
+wait
+
+# Update package list
+sudo apt-get update
